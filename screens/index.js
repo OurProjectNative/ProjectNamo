d89a1ec7aeb36079046e4330cb5ba0ba6e49ff4f
vincentestenzo113 <vincentestenzo1@gmail.com>
2023-10-26 21:56:11 +0800

    first commit

---
+++ screens/index.js
@@ -0,0 +1,11 @@
+import Login from "./Login";
+import Welcome from "./Welcome";
+import Signup from "./Signup";
+import Forgot from "./Forgot";
+
+export {
+    Login,
+    Welcome,
+    Signup,
+    Forgot
+}
