5a6f55ff147f906257aa41ec508acbdaddd61bce
vincentestenzo113 <vincentestenzo1@gmail.com>
2023-10-28 12:57:11 +0800

    removed some code

--- screens/Forgot.js
+++ screens/Forgot.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, Pressable, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, Pressable, TextInput, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from "react-native-safe-area-context";
 import COLORS from '../constants/colors';
